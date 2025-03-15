6fc544aac028e8092652056652bf26026277c921
KiaLH <kia.mooc@gmail.com>
2025-03-15 19:07:31 +0100

    Blob main file changed

--- modules/blob/main.tf
+++ modules/blob/main.tf
@@ -1,9 +1,21 @@
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "${lower(var.storage_account_name)}${random_string.suffix.result}"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
+  min_tls_version          = "TLS1_2"
+  tags                     = merge(var.tags, { Module = "blob" })
+}
 
-  tags = var.tags
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
 }
