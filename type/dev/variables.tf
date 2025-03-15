37339648399f38a28de5373533a4311cc9d5fc15
KiaLH <kia.mooc@gmail.com>
2025-03-15 16:38:43 +0100

    Modified dev variables file

--- type/dev/variables.tf
+++ type/dev/variables.tf
@@ -1,19 +1,16 @@
 variable "location" {
   description = "Azure region for the dev environment"
   type        = string
-  default     = "eastus"
 }
 
 variable "admin_username" {
   description = "Admin username for the VM"
   type        = string
-  default     = "adminuser"
 }
 
 variable "admin_ssh_public_key" {
   description = "SSH public key for the VM"
   type        = string
-  default     = file("~/.ssh/id_rsa.pub")  # Reads your SSH public key from a local file
 }
 
 # Optional: If you wish to explicitly pass subscription and tenant information:
